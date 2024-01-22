91e1aa68f43fa4f99a1cf314b6a68d7096b01aab
Caigel <97202202+Caigel@users.noreply.github.com>
2024-01-22 15:07:02 -0500

    Add files via upload

---
+++ 3.1-3.6.py
@@ -0,0 +1,12 @@
+seconds_in_a_minute = 60
+minutes_in_a_hour = 60
+seconds_in_a_hour = seconds_in_a_minute * minutes_in_a_hour
+seconds_per_day = seconds_in_a_hour * 24
+
+print(seconds_per_day // seconds_in_a_hour)
+# 3.1, 3600
+# 3.2, 
+# 3.3, 86,400
+# 3.4, 
+# 3.5, 
+# 3.6, Yes
